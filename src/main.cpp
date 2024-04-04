1a3c8baadde7f6efddd5533683140814408b2f75
Sir Dystic <4115891-sirdystic@users.noreply.gitlab.com>
2024-04-04 09:33:04 -0700

    Change ESP_LOGD() to Serial.println()

--- src/main.cpp
+++ src/main.cpp
@@ -13,41 +13,41 @@ Arducam_Mega myCAM(CS);
 
 void setupCameraSettings()
 {
-    ESP_LOGD(_TAG, "SetupCameraSettings");
+    Serial.println("SetupCameraSettings");
     // Turn off auto exposure and set absolute exposure
     myCAM.setAutoExposure(1);
     // myCAM .setAbsoluteExposure(100);
-    ESP_LOGD(_TAG, "Auto Exposure Set to 0 and Absolute Exposure Set to 100");
+    Serial.println("Auto Exposure Set to 0 and Absolute Exposure Set to 100");
 
     // Turn off auto white balance and set it to a fixed mode
     myCAM.setAutoWhiteBalance(1);
     // myCAM.setAutoWhiteBalanceMode(CAM_WHITE_BALANCE_MODE_SUNNY);
-    ESP_LOGD(_TAG, "Auto White Balance turned off and set to Sunny mode");
+    Serial.println("Auto White Balance turned off and set to Sunny mode");
 
     // Set image saturation, exposure value, and contrast
     myCAM.setSaturation(CAM_STAURATION_LEVEL_DEFAULT);
-    ESP_LOGD(_TAG, "Saturation set to default");
+    Serial.println("Saturation set to default");
 
     myCAM.setEV(CAM_EV_LEVEL_DEFAULT);
-    ESP_LOGD(_TAG, "Exposure Value set to default");
+    Serial.println("Exposure Value set to default");
 
     myCAM.setContrast(CAM_CONTRAST_LEVEL_DEFAULT);
-    ESP_LOGD(_TAG, "Contrast set to default");
+    Serial.println("Contrast set to default");
 
     // Set image brightness and sharpness
     myCAM.setBrightness(CAM_BRIGHTNESS_LEVEL_DEFAULT);
-    ESP_LOGD(_TAG, "Brightness set to default");
+    Serial.println("Brightness set to default");
 
     myCAM.setSharpness(CAM_SHARPNESS_LEVEL_AUTO);
-    ESP_LOGD(_TAG, "Sharpness set to auto");
+    Serial.println("Sharpness set to auto");
 
     // Set color effects to none
     myCAM.setColorEffect(CAM_COLOR_FX_NONE);
-    ESP_LOGD(_TAG, "Color Effect set to none");
+    Serial.println("Color Effect set to none");
 
     // Set the image quality to default
     myCAM.setImageQuality(HIGH_QUALITY);
-    ESP_LOGD(_TAG, "Image Quality set to default");
+    Serial.println("Image Quality set to default");
 }
 
 uint8_t *captureImage(size_t *length)
@@ -59,7 +59,7 @@ uint8_t *captureImage(size_t *length)
         return nullptr;
     }
     *length = myCAM.getTotalLength();
-    ESP_LOGD(_TAG, "Image size (bytes): %d", *length);
+    Serial.println("Image size (bytes): " + String(*length));
     uint8_t *imageBuffer = new uint8_t[*length];
     if (imageBuffer == nullptr)
     {
@@ -85,33 +85,47 @@ uint8_t *captureImage(size_t *length)
 }
 
 void setup() {
-    ESP_LOGD(_TAG, "setup() entered");
 
-    ESP_LOGD(_TAG, "Calling Arducam_Mega.begin()");
+    Serial.begin(115200);
+    // Connect to WiFi
+    while (!Serial)
+    {
+    } // Wait for serial port to connect
+
+    Serial.println("setup() entered");
+
+    Serial.println("Calling Arducam_Mega.begin()");
     myCAM.begin();
-    ESP_LOGD(_TAG, "Arducam_Mega.begin() called");
+    Serial.println("Arducam_Mega.begin() called");
 
-    ESP_LOGD(_TAG, "Calling setupCameraSettings()");
+    Serial.println("Calling setupCameraSettings()");
     setupCameraSettings();
-    ESP_LOGD(_TAG, "setupCameraSettings() called");
+    Serial.println("setupCameraSettings() called");
 
-    ESP_LOGD(_TAG, "setup() finished");
+    Serial.println("setup() finished");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-    ESP_LOGD(_TAG, "Free internal heap: %d  Minimum free heap size: %d",
-        esp_get_free_internal_heap_size(), esp_get_minimum_free_heap_size());
+    String message;
+    message = "Free internal heap: " +
+        String(esp_get_free_internal_heap_size()) +
+        "  Minimum free heap size: " + 
+        String(esp_get_minimum_free_heap_size());
+    Serial.println(message);
 
-    ESP_LOGD(_TAG, "Capturing image...");
+    Serial.println("Capturing image...");
     size_t length = 0;
     uint8_t* imageData = captureImage(&length);
-    ESP_LOGD(_TAG, "Image captured, length %u", length);
+    Serial.println("Image captured, length " + String(length));
     delete[] imageData;
 
-    ESP_LOGD(_TAG, "Free internal heap: %d  Minimum free heap size: %d",
-        esp_get_free_internal_heap_size(), esp_get_minimum_free_heap_size());
+    message = "Free internal heap: " +
+        String(esp_get_free_internal_heap_size()) +
+        "  Minimum free heap size: " + 
+        String(esp_get_minimum_free_heap_size());
+    Serial.println(message);
 
     delay(2000);
 }
